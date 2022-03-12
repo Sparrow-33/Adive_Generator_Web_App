34aa0b1aa23b4efac39952f1d27a94a6a1ea534e
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-03-12 13:31:06 +0100

    Button fixed

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,16 @@
-let action = document.querySelector('.action');
-const num = document.getElementsByClassName('id');
-let para = document.getElementsByClassName('quote');
+// const num = document.getElementsByClassName('id');
+// const para = document.getElementsByClassName('quote');
 
-    // action.addEventListener('click', async ()=>{
-    //      let responce = await fetch('https://api.adviceslip.com/advice')
-    //      let data = await responce.json()
-
-    //      console.log(data)
-       
-    // })
-console.log(num)
-    const quote = async ()=> {
-         let responce = await fetch('https://api.adviceslip.com/advice')
-         let data = await responce.json()
+   
+    const getAdvice = async () => {
+         let resp = await fetch('https://api.adviceslip.com/advice')
+         let data = await resp.json()
          
-         num[0].innerHTML = data.slip.id
-         para[0].innerHTML = data.slip.advice
-        console.log(data)
+         document.getElementById('id').innerHTML = `# ${data.slip.id}`
+         document.getElementById('quote').innerHTML = `" ${data.slip.advice} "`
+
+         console.log(data.slip.advice)
 
                            }
 
-    quote()
+        window.onload(quote())  
