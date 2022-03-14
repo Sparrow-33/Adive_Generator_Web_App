74b973b51fa996b942f8b952f1f7d6d36cf972e8
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-03-14 11:42:57 +0100

    cash deleted

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,20 @@
 // const num = document.getElementsByClassName('id');
 // const para = document.getElementsByClassName('quote');
 
-   
+
     const getAdvice = async () => {
-         let resp = await fetch('https://api.adviceslip.com/advice')
-         let data = await resp.json()
+         var resp = await fetch('https://api.adviceslip.com/advice', {
+            cache: "no-cache"
+          })
+         var data = await resp.json()
          
          document.getElementById('id').innerHTML = `# ${data.slip.id}`
          document.getElementById('quote').innerHTML = `" ${data.slip.advice} "`
-
          console.log(data.slip.advice)
 
-                           }
 
-        window.onload(quote())  
+      //   console.log(data.slip.advice) 
+
+    }
+    
+    getAdvice()
